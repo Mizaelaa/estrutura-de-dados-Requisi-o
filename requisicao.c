5458f283e0c8482e567be1d34b36b06477dda861
Mizaelaa <116915423+Mizaelaa@users.noreply.github.com>
2025-04-23 21:55:39 -0300

    Update requisicao.c

--- requisicao.c
+++ requisicao.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "requisicao.h"
 
-// Corrigir nome da função para 'cria_requisicao' para corresponder ao teste.
+
 Requisicao* cria_requisicao(const char *nome, int inscricao, const char *procedimento) {
     Requisicao *r = (Requisicao *)malloc(sizeof(Requisicao));
     if (r != NULL) {
@@ -22,7 +22,7 @@ void libera(Requisicao *r) {
     free(r);
 }
 
-// Corrigir o tipo de retorno para char* (já que é uma string)
+
 char* get_nome(Requisicao *r) {
     return r->nome;  // Retorna uma string
 }
@@ -31,7 +31,7 @@ int get_inscricao(Requisicao *r) {
     return r->inscricao;
 }
 
-// Corrigir o tipo de retorno para char* (já que é uma string)
+
 char* get_procedimento(Requisicao *r) {
     return r->procedimento; // Retorna uma string
 }
